876f16159372ef62a8bd89fc13eba3e57aae72ca
Alaatohamy <alaatohamy4@gmail.com>
2019-08-21 02:47:46 +0200

    Feat: Add search by name

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import CardList from './components/card-list/card-list';
+import Input from './components/search-box/search-box';
 
 class App extends Component{
 
   state ={
     monsters: [],
-    error: ''
+    error: '',
+    searchWord: ''
   }
 
   componentDidMount(){
@@ -18,14 +20,22 @@ class App extends Component{
     .catch(err => this.setState({error: 'Something wrong happened'}));
   }
 
+  searchHandleChange = e => {
+    this.setState({searchWord: e.target.value });
+  }
+
   render(){
-    const {monsters, error} = this.state;
+    const {monsters, error, searchWord} = this.state;
+    const listedMonsters = 
+      searchWord ? monsters.filter(monster => monster.name.toLowerCase().includes(searchWord.toLowerCase())) : monsters ;
+
     return (
       <div className="App">
+        <Input handleChange={this.searchHandleChange} placeholder='Search by name' />
         <section>
         {
           error ? error : 
-          <CardList monsters={monsters} />
+          <CardList monsters={listedMonsters} />
         }
         </section>
       </div>
