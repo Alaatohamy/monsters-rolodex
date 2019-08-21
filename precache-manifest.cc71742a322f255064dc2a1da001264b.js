9a51b0326fb91b7817ad6cdc1bae0381e40c7487
Alaatohamy <alaatohamy4@gmail.com>
2019-08-21 04:09:59 +0200

    Updates

---
+++ precache-manifest.cc71742a322f255064dc2a1da001264b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f30a18663275661c5c855817d5909b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "191d2a61df9e604b55d8",
+    "url": "/monsters-rolodex/static/css/main.d741e5b6.chunk.css"
+  },
+  {
+    "revision": "daa5b256aafc1115af2c",
+    "url": "/monsters-rolodex/static/js/2.8dbc33dc.chunk.js"
+  },
+  {
+    "revision": "191d2a61df9e604b55d8",
+    "url": "/monsters-rolodex/static/js/main.34b55dd0.chunk.js"
+  },
+  {
+    "revision": "1bdd311f77ec621ee4ea",
+    "url": "/monsters-rolodex/static/js/runtime~main.adca24da.js"
+  }
+]);
